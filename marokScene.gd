485088016fe1a7bf26a617c6da524ec751cb9b86
marko <marko.m.prijic@gmail.com>
2024-03-21 15:16:55 +0000

    piano sound

---
+++ marokScene.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+func _on_button_pressed():
+	$AudioStreamPlayer2D.play()
+	pass # Replace with function body.
+
+
+
+func _on_h_slider_value_changed(value):
+	var pitch:AudioEffectPitchShift = AudioServer.get_bus_effect(0,0)
+	
+	pitch.pitch_scale = $HSlider.value
+	pass # Replace with function body.
