5e72d459411ac479e1d0ced394fee0696959a91d
marko <marko.m.prijic@gmail.com>
2024-03-27 18:22:28 +0000

    scene1

--- marokScene.gd
+++ marokScene.gd
@@ -5,8 +5,6 @@ func _on_button_pressed():
 	$AudioStreamPlayer2D.play()
 	pass # Replace with function body.
 
-
-
 func _on_h_slider_value_changed(value):
 	var pitch:AudioEffectPitchShift = AudioServer.get_bus_effect(0,0)
 	
