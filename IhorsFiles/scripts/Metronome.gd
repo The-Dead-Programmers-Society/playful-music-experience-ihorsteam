105606fca511f0cdb6e388f4847d273acff6aa04
SAW3production <105048323+SAW3production@users.noreply.github.com>
2024-04-19 21:29:36 +0100

    proper pad

--- IhorsFiles/scripts/Metronome.gd
+++ IhorsFiles/scripts/Metronome.gd
@@ -12,14 +12,24 @@ var time:float  = 0.0
 @onready var main_sound = $"../MainSound"
 @export var array2D = []
 var current_row = 0
-@onready var panel_3 = $"../Panel3"
-@onready var panel_4 = $"../Panel4"
+@onready var panel_3 = $"../Pads/Panel3"
+@onready var panel_4 = $"../Pads/Panel4"
+@onready var panel_5 = $"../Pads/Panel5"
+@onready var panel_6 = $"../Pads/Panel6"
+@onready var panel_7 = $"../Pads/Panel7"
+@onready var panel_8 = $"../Pads/Panel8"
+@onready var panel_9 = $"../Pads/Panel9"
 
 var repeat:bool  = false
 
 func _ready():
 	array2D.append(panel_3.buttons)
 	array2D.append(panel_4.buttons)
+	array2D.append(panel_5.buttons)
+	array2D.append(panel_6.buttons)
+	array2D.append(panel_7.buttons)
+	array2D.append(panel_8.buttons)
+	array2D.append(panel_9.buttons)
 func _process(delta):
 	if(repeat):
 		time +=delta
@@ -27,21 +37,32 @@ func _process(delta):
 		if time >= bps:
 			
 			time -=bps
+			array2D[current_row][0].panel_turnOn()
 			for i in array2D[current_row]:
 				if(i.can_play):
 					i.audio.play()
+			array2D[current_row][0].panel_turnOff()
 			current_row +=1
 		if(current_row > array2D.size() - 1):
 			current_row = 0
+			
 
 
 
 
 func _on_toggled(toggled_on):
 	repeat = toggled_on
+	current_row = 0
 	pass # Replace with function body.
 
 
 func _on_v_slider_value_changed(value):
 	bpm = value
+	pass # Replace with function body.
+
+
+func _on_button_pressed():
+	for i in array2D.size():
+		for j in array2D[i].size():
+			array2D[i][j].toggleOff()
 	pass # Replace with function body.
--- IhorsFiles/scripts/button.gd
+++ IhorsFiles/scripts/button.gd
@@ -4,16 +4,29 @@ class_name AudioButton
 var can_play:bool = false
 @export_range(0,12) var current_note:int = 0 
 @onready var audio = $AudioStreamPlayer2D
+@onready var panel_3 = $".."
+var current_theme
+
 
 func _ready():
 	pitch_sound(current_note)
+	current_theme = panel_3.get_theme_stylebox("panel")
 	
 func pitch_sound(note: float):
 	var pitch = pow(2, note/12.0)
 	audio.pitch_scale = pitch
-	
 
 
 func _on_toggled(toggled_on):
 	can_play = toggled_on
 	pass # Replace with function body.
+func toggleOff():
+	button_pressed = false
+func panel_turnOn():
+	var style:StyleBoxFlat = StyleBoxFlat.new()
+	style.bg_color =  Color.BLACK
+	style.bg_color.a = 0.5
+	panel_3.add_theme_stylebox_override ("panel", style)
+func panel_turnOff():
+	await get_tree().create_timer(0.2).timeout
+	panel_3.add_theme_stylebox_override ("panel", current_theme)
