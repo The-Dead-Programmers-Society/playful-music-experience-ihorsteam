9ca47f34fd422c8a8a7ca2bb55189fb65b40fc3f
alexhoka <d22124392@mytudublin.ie>
2024-04-21 01:10:25 +0100

    recorder implemented

--- AlexFiles/scripts/Recorder.gd
+++ AlexFiles/scripts/Recorder.gd
@@ -1,1 +1,53 @@
 extends Panel
+@onready var play = $Play
+@onready var start = $Start
+var isRecording: bool = false
+var isPlaying: bool = false
+var recording = []
+var current_note_in_recording = 0
+
+@onready var bpm:float  = 100:
+	get:
+		return bpm
+	set(value):
+		bps = 60/value
+		bpm = value
+		
+@onready var bps:float  = 60/bpm
+var time:float  = 0.0
+
+func _process(delta):
+	if(isPlaying):
+		time +=delta
+		if time >= bps:
+			time -=bps
+			recording[current_note_in_recording].audio.play()
+			current_note_in_recording += 1
+		if(current_note_in_recording>recording.size() - 1):
+			current_note_in_recording = 0
+			
+
+
+func _on_start_toggled(toggled_on):
+	isRecording = toggled_on
+	if(isRecording):
+		start.text = "Recording..."
+	else:
+		start.text = "Start Recording"
+		
+	pass # Replace with function body.
+
+
+
+func _on_play_toggled(toggled_on):
+	isPlaying = toggled_on
+	if(isPlaying):
+		play.text = "Playing"
+	else:
+		play.text = "Play"
+	pass # Replace with function body.
+
+
+func _on_v_slider_value_changed(value):
+	bpm = value
+	pass # Replace with function body.
--- AlexFiles/scripts/SoundButton.gd
+++ AlexFiles/scripts/SoundButton.gd
@@ -2,11 +2,14 @@ extends Button
 
 @onready var audio = $AudioStreamPlayer2D
 @export var current_note := 0
+@onready var recorder = $"../../Recorder"
 
 func _ready():
 	pitch_sound(current_note)
 
 func _on_pressed():
+	if(recorder.isRecording):
+		recorder.recording.append(get_node(self.get_path()))
 	audio.play()
 	pass # Replace with function body.
 
