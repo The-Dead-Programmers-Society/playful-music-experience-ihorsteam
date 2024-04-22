07f9d3878892ce0a8b274113ac87b4fd45da4223
marko <marko.m.prijic@gmail.com>
2024-04-22 13:21:28 +0100

    ow

--- MarkoFiles/Scripts/marokScene.gd
+++ MarkoFiles/Scripts/marokScene.gd
@@ -2,11 +2,17 @@ extends Node2D
 
 
 func _on_button_pressed():
-	$AudioStreamPlayer2D.play()
+	$dogbark.play()
 	pass # Replace with function body.
+	
 
 func _on_h_slider_value_changed(value):
 	var pitch:AudioEffectPitchShift = AudioServer.get_bus_effect(0,0)
 	
 	pitch.pitch_scale = $HSlider.value
 	pass # Replace with function body.
+	
+func _process(_delta):
+	if Input.is_action_pressed("bark_sound"):
+		$dogbark.play()
+
